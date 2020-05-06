83a34c65748e8286b5a6ded0a4c8e12a601ba14e
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-05-06 14:17:57 +0400

    add npm package to handle async routes and errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -17,7 +17,7 @@ router.post(
       })
       .withMessage('Password must be between 4 and 30 characters')
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     // validationResult is result from error validation middleware
     const errors = validationResult(req);
 
