a2f968ba0773926de5c39d1c79fdfbd13d4db30e
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-05-10 17:53:11 +0400

    add cookies and JWT for auth -> need to add secret with k8s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose, { mongo } from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -12,7 +13,17 @@ import { NotFoundError } from './errors/not-found-error';
 import e from 'express';
 
 const app = express();
+// tell express that it is behind a https proxy of ingress-nginx and to trust it
+app.set('trust proxy', true);
 app.use(json());
+
+// disable cookie encryption and allow https-only cookie
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
