60193df21e964792f480d8a36bd5dd443c68416f
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-05-17 23:24:31 +0400

    set middleware and write tests with TDD approach

--- ticket/src/app.ts
+++ ticket/src/app.ts
@@ -2,20 +2,30 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@jnch-microservice-tickets/common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser
+} from '@jnch-microservice-tickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 // tell express that it is behind a https proxy of ingress-nginx and to trust it
 app.set('trust proxy', true);
 app.use(json());
 
-// disable cookie encryption and allow https-only cookie
+// set cookie with JWT, disable cookie encryption and allow https-only cookie
 app.use(
   cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
   })
 );
+
+// set currentUser to request object, need to call before require-auth middleware
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
