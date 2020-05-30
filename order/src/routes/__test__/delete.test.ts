778b7c6f72d66b92f072e3c19f83287ee30ca8e3
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-05-31 01:27:09 +0400

    add delete order route and tests

---
+++ order/src/routes/__test__/delete.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+
+it('marks an order as cancelled', async () => {
+  const ticket = Ticket.build({
+    title: 'parking',
+    price: 20
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  const deletedOrder = await Order.findById(order.id);
+
+  expect(deletedOrder?.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits an order cancelled event');
--- order/src/routes/delete.ts
+++ order/src/routes/delete.ts
@@ -1,9 +1,40 @@
 import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError
+} from '@jnch-microservice-tickets/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+/**
+ * Update order status to cancel instead of deleting the record => can keep track of all orders
+ * a worker job can then run and clear any unwanted orders if needed
+ */
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // need to publish order:cancelled event
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
