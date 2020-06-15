5ff8172dcb633327826c4d1d31acdb92f7d12f0f
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-06-15 10:30:32 +0400

    initial commit for expiration service

---
+++ expiration/src/index.ts
@@ -0,0 +1,41 @@
+import { natsClient } from './nats-client';
+
+const start = async () => {
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  try {
+    await natsClient.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsClient.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsClient.client.close());
+    process.on('SIGTERM', () => natsClient.client.close());
+
+    process.on('unhandledRejection', (reason, p) => {
+      // @ts-ignore
+      console.log('Unhandled Rejection at: Promise', p, 'reason:', reason.stack);
+      // application specific logging, throwing an error, or other logic here
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
