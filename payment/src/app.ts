6c82b1066f4212b71e38158a4ebdfdd7e0a17191
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-06-21 12:33:34 +0400

    init payment service

---
+++ payment/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser
+} from '@jnch-microservice-tickets/common';
+
+const app = express();
+// tell express that it is behind a https proxy of ingress-nginx and to trust it
+app.set('trust proxy', true);
+app.use(json());
+
+// set cookie with JWT, disable cookie encryption and allow https-only cookie
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+
+// set currentUser to request object, need to call before require-auth middleware
+app.use(currentUser);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payment/src/index.ts
@@ -0,0 +1,64 @@
+import mongoose, { mongo } from 'mongoose';
+
+import { app } from './app';
+import { natsClient } from './nats-client';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  try {
+    await natsClient.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsClient.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsClient.client.close());
+    process.on('SIGTERM', () => natsClient.client.close());
+
+    process.on('unhandledRejection', (reason, p) => {
+      // @ts-ignore
+      console.log('Unhandled Rejection at: Promise', p, 'reason:', reason.stack);
+      // application specific logging, throwing an error, or other logic here
+    });
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    console.log('Connected to mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+  });
+};
+
+start();
