81199b5412b6df9c34545a3a91a313202df6eff2
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-06-27 17:52:25 +0400

    add ordershow page

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,32 @@
+import { useState, useEffect } from 'react';
+
 const OrderShow = ({ order }) => {
-  console.log(order);
-  return <div>OrderShow</div>;
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const time = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(time / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    /**
+     * Whenever we return a function from useEffect:
+     * this return function will only run if we navigate away OR stop showing this component, if we have empty [] in useEffect
+     * this return function will be called if we navigate away OR on any rerender, if we have [valid dependency/changing variable to check for rerendering],
+     */
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return <div>Time left to complete payment: {timeLeft} seconds</div>;
 };
 
 OrderShow.getInitialProps = async (context, axiosClient, currentUser) => {
