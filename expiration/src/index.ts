b3ab092878226b59f387cb781414fc0ab4d8a607
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-06-28 18:05:05 +0400

    small changes to all services to test auto deployment on PR merge

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,8 @@ import { natsClient } from './nats-client';
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
+  console.log('Auth-srv starting up....');
+
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
