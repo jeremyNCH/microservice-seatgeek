c33aefb44ebcc0189a74d61a201d20822086cc11
Jeremy Afoke <jnchafok@uwaterloo.ca>
2020-06-28 14:05:22 +0400

    format line

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { app } from './app';
 
 const start = async () => {
   console.log('Auth-srv starting up...');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
